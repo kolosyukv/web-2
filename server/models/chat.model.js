1c876df76e3f9be6826523e00b46ff672da25a92
Vlad K <kolosyuk99@mail.ru>
2024-03-13 20:53:30 +0300

    Task 2

---
+++ server/models/chat.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const Chat = mongoose.model("Chat", new mongoose.Schema({
+    chatname: String,
+    participants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
+}))
+
+module.exports = Chat;
